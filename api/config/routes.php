7e0c4f79ca80515c71f9ed0e9a5668e284e9d05f
raph <raphrien@gmail.com>
2022-04-21 08:21:11 -0400

    Removed old routes

--- api/config/routes.php
+++ api/config/routes.php
@@ -8,20 +8,6 @@ use Slim\App;
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
 
-    $app->post('/users', \App\Action\UserCreateAction::class);
-
-    $app->get('/users', \App\Action\ListUsersAction::class);
-
-    $app->get('/users/{id}', \App\Action\ListUserFromIDAction::class);
-
-    $app->put('/users/{id}', \App\Action\ModifyUserFromIDAction::class);
-
-    $app->delete('/users/{id}', \App\Action\DeleteUserFromIDAction::class);
-
-    $app->get('/cle_api', \App\Action\CreateAPIKeyAction::class);
-
-    $app->get('/livres', \App\Action\ListBooksAction::class);
-
     // Documentation de l'api
     $app->get('/docs', \App\Action\Docs\SwaggerUiAction::class);
 
